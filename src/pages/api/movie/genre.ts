0238fe3d35b4ea65f705476d29b69a59973e9656
Nilotpal Gupta <nilotpalgupta0701@gmail.com>
2025-05-19 20:34:33 +0530

    h

--- src/pages/api/movie/genre.ts
+++ src/pages/api/movie/genre.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { tmdbFetch } from "@/pages/lib/tmdb";
+import { tmdbFetch } from "@/lib/tmdb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { genreId } = req.query;
