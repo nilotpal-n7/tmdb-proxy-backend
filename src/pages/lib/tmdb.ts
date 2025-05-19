c2dee03fe809d2e62fef7d98e7968784fe3562e1
Nilotpal Gupta <nilotpalgupta0701@gmail.com>
2025-05-19 20:19:54 +0530

    done

--- src/pages/lib/tmdb.ts
+++ src/pages/lib/tmdb.ts
@@ -4,6 +4,7 @@ export const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 export async function tmdbFetch(path: string, query: string = "") {
   const url = `${TMDB_BASE_URL}${path}?api_key=${TMDB_API_KEY}&language=en-US&${query}`;
+  console.log('URL: ', url);
   const res = await fetch(url);
   if (!res.ok) throw new Error(`TMDB error: ${res.status}`);
   return res.json();
