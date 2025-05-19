5049ba7be04196cbe2f06cd9e439d72b3eec0935
Nilotpal Gupta <nilotpalgupta0701@gmail.com>
2025-05-20 03:34:42 +0530

    fixed await

--- src/pages/api/search/search.ts
+++ src/pages/api/search/search.ts
@@ -5,9 +5,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (!search) return res.status(400).json({ error: "Missing search" });
 
   try {
-    const data = await fetch(`https://api.themoviedb.org/3/search/multi?query=${search}&api_key=afb47585537b86dec25d50d1ee9d31e9`);
-    if (!data.ok) throw new Error(`TMDB error: ${res.status}`);
-    res.status(200).json(data.json());
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/multi?query=${encodeURIComponent(
+        search as string
+      )}&api_key=afb47585537b86dec25d50d1ee9d31e9`
+    );
+
+    if (!response.ok) throw new Error(`TMDB error: ${response.status}`);
+    
+    const data = await response.json();
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: (err as Error).message });
   }
